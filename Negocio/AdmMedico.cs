6cdb56f9a60f941ad6e4718e40827ddbe2b67914
samueltribulo <samueltribulo@gmail.com>
2023-01-12 19:16:05 -0300

    AdmMedico

---
+++ Negocio/AdmMedico.cs
@@ -0,0 +1,46 @@
+﻿using Entidades;
+
+namespace Negocio
+{
+    public static class AdmMedico
+    {
+
+        static List<Medico> Listar()
+        {
+
+            //TODO Falta logica de Listar para Medico.
+
+            return null;
+        }
+
+        static List<Medico> ListarEspecialidad (string especialidad)
+        {
+
+            //TODO Falta logica de ListarEspecialidad para Medico.
+
+            return null;
+        }
+
+        static int Insertar(Medico medico)
+        {
+            //TODO Falta logica de Insertar para Medico.
+
+            return 0;
+        }
+
+        static int Eliminar(int id)
+        {
+            //TODO Falta logica de Eliminar para Medico.
+
+            return 0;
+        }
+
+        static Medico TraerUno(int id) {
+
+            //TODO Falta logica de TraerUno para Medico.
+
+            return null;
+        }
+
+    }
+}
