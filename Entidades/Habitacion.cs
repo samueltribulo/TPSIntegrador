89404f2fc99ca070a819000ccad0229e80bce6a6
samueltribulo <samueltribulo@gmail.com>
2023-01-12 19:01:04 -0300

    Agregar archivos de proyecto.

---
+++ Entidades/Habitacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Habitacion
+    {
+        public int ID { get; set; }
+        public int Numero { get; set; }
+        public string Estado { get; set; }
+
+    }
+}
---
+++ Entidades/Persona.cs
@@ -0,0 +1,13 @@
+﻿namespace Entidades
+{
+    public abstract class Persona
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Domicilio { get; set; }
+        public int Telefono { get; set; }
+        public string Email { get; set; }
+
+    }
+}
